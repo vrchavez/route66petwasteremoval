55118a32a57954b61afb1dd4790e8ecd66942ae7
vrchavez <vrchavez07@gmail.com>
2026-03-05 16:24:14 -0800

    Added routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'pricing',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'get-in-touch',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'thank-you',
     renderMode: RenderMode.Prerender
   }
 ];
